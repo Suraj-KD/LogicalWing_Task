f15efb2526b4cc242bca26554318d281a9ab5530
Suraj-K-Dubey <surajdubey302@gmail.com>
2019-01-04 22:49:17 +0530

    Modified employee Model

--- Suraj_03_01_2019_Practical/employee/models.py
+++ Suraj_03_01_2019_Practical/employee/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils import timezone
 import datetime
 
 # Create your models here.
 class Employee(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    DOJ = models.DateField(default=datetime.date.today())
+    DOJ = models.DateField(default=timezone.now)
     address = models.TextField(max_length=100)
     city = models.CharField(max_length=20)
     DOB = models.DateField()
